2b39399a04c88de86541efa5b265245549db6ce3
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-04-14 10:48:41 -0700

    Banish Boredom: 7

--- banish-Boredom/main.js
+++ banish-Boredom/main.js
@@ -40,60 +40,63 @@ const nopeButton = document.getElementById("nope-button");
 nopeButton.onclick = onNopeClicked;
 
 const onLikeClicked = () => {
-  const savedActivitiesString = localStorage.getItem("activities");
-
-  const savedActivities = JSON.parse(savedActivitiesString) ?? [];
-
-  savedActivities.push(currentActivity);
-
-  localStorage.setItem("activities", JSON.stringify(savedActivities));
-
-  onBoredClicked(); // a forth place we call this!
-};
+    const savedActivitiesString = localStorage.getItem("activities");
+  
+    const savedActivities = JSON.parse(savedActivitiesString) ?? [];
+  
+    savedActivities.push(currentActivity);
+  
+    localStorage.setItem("activities", JSON.stringify(savedActivities));
+  
+    fetchNewActivity();
+    renderList();
+  };
 
 const likeButton = document.getElementById("like-button");
 likeButton.onclick = onLikeClicked;
 
-window.onload = () => {
-  const savedActivitiesString = localStorage.getItem("activities");
-
-  const savedActivities = JSON.parse(savedActivitiesString) ?? [];
-
-  savedActivities.forEach((savedActivity) => {
-    const newCard = placeholderCard.cloneNode(true);
-    console.log(newCard);
-    newCard.id = savedActivity.key;
-
-    const newCardActivity = newCard.querySelector(".activity");
-    newCardActivity.innerText = savedActivity.activity;
-
-    const newCardType = newCard.querySelector(".type");
-    newCardType.innerText = savedActivity.type;
-
-    const newCardParticipants = newCard.querySelector(".participants");
-    newCardParticipants.innerText = savedActivity.participants;
-
-    const newCardPrice = newCard.querySelector(".price");
-    newCardPrice.innerText = savedActivity.price;
-
-    if (savedActivity.link) {
-      const newCardLink = newCard.querySelector(".link");
-      const newCardLinkContainer = newCard.querySelector(".link-container");
-      console.log(newCardLinkContainer);
-      newCardLink.href = savedActivity.link;
-      newCardLinkContainer.hidden = false;
-    }
-
-    const newCardDeleteButton = newCard.querySelector(".delete-button");
-    newCardDeleteButton.onclick = () => {
-      console.log("delete: ", savedActivity.key);
-    };
-
-    list.appendChild(newCard);
-  });
-
-  onBoredClicked();
-};
+const renderList = () => {
+    const savedActivitiesString = localStorage.getItem("activities");
+  
+    const savedActivities = JSON.parse(savedActivitiesString) ?? [];
+  
+    savedActivities.forEach((savedActivity) => {
+      const newCard = placeholderCard.cloneNode(true);
+      console.log(newCard);
+      newCard.id = savedActivity.key;
+  
+      const newCardActivity = newCard.querySelector(".activity");
+      newCardActivity.innerText = savedActivity.activity;
+  
+      const newCardType = newCard.querySelector(".type");
+      newCardType.innerText = savedActivity.type;
+  
+      const newCardParticipants = newCard.querySelector(".participants");
+      newCardParticipants.innerText = savedActivity.participants;
+  
+      const newCardPrice = newCard.querySelector(".price");
+      newCardPrice.innerText = savedActivity.price;
+  
+      if (savedActivity.link) {
+        const newCardLink = newCard.querySelector(".link");
+        const newCardLinkContainer = newCard.querySelector(".link-container");
+  
+        newCardLink.href = savedActivity.link;
+        newCardLinkContainer.hidden = false;
+      }
+  
+      const newCardDeleteButton = newCard.querySelector(".delete-button");
+      newCardDeleteButton.onclick = () => {
+        deleteActivity(savedActivity.key);
+      };
+  
+      list.appendChild(newCard);
+    });
+  };
+  window.onload = () => {
+    renderList();
+    fetchNewActivity();
+  };
 
 const deleteActivity = (key) => {
   const savedActivitiesString = localStorage.getItem("activities");
@@ -105,8 +108,7 @@ const deleteActivity = (key) => {
   );
 
   localStorage.setItem("activities", JSON.stringify(updatedActivities));
-  const deletedActivity = document.getElementById(key);
-  deletedActivity.remove();
+  renderList();
 };
 newCardDeleteButton.onclick = () => {
   deleteActivity(savedActivity.key);
