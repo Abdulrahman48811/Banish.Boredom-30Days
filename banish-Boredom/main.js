983a10fe7688e71c413cb406149d6ce341524ea4
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-04-13 17:54:53 -0700

    Banish Boredom: 3

--- banish-Boredom/main.js
+++ banish-Boredom/main.js
@@ -5,24 +5,51 @@ const participants = document.getElementById("participants");
 const price = document.getElementById("price");
 const link = document.getElementById("link");
 
+let currentActivity;
+
 const onBoredClicked = () => {
     fetch("https://www.boredapi.com/api/activity/")
     .then((res) => res.json())
     .then((data) => {
+        console.log(data.link);
         activity.innerText = data.activity;
         type.innerText = data.type;
         participants.innerText = data.participants;
         price.innerText = data.price;
         if (data.link.length) {
-            link.href = data.link;
-            linkContainer.hidden = false;
-          } else {
-            linkContainer.hidden = true;
-          }
-    })
+          link.href = data.link;
+          linkContainer.hidden = false;
+        } else {
+          linkContainer.hidden = true;
+        }
+        currentActivity = data;
+      })
     .catch((error) => console.log(error));
   };
   
   const boredButton = document.getElementById("bored-button");
   boredButton.onclick = onBoredClicked;
+
+  const onNopeClicked = () => {
+    onBoredClicked();
+  };
   
+  const nopeButton = document.getElementById("nope-button");
+  nopeButton.onclick = onNopeClicked;
+
+  const onLikeClicked = () => {
+    const savedActivitiesString = localStorage.getItem("activities");
+  
+    const savedActivities = JSON.parse(savedActivitiesString) ?? [];
+  
+    savedActivities.push(currentActivity);
+  
+    localStorage.setItem("activities", JSON.stringify(savedActivities));
+  
+    onBoredClicked(); // a forth place we call this!
+  };
+  
+  const likeButton = document.getElementById("like-button");
+  likeButton.onclick = onLikeClicked;
+
+window.onload = onBoredClicked;
